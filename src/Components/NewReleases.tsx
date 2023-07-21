a70259e4cfa516435051bee45f3b8feb558ec628
andrew-trotter <andrewjtrotter@gmail.com>
2023-07-21 15:45:02 +1200

    made ready for data

---
+++ src/Components/NewReleases.tsx
@@ -0,0 +1,17 @@
+import * as elements from 'typed-html'
+
+function NewReleases() {
+  
+  return (
+    <div class="p-2 bg-malbik-gray border-2 border-gray-400 rounded-md">
+        <h1 class="text-malbik-red font-bold text-xl">
+          Outstanding New Releases
+        </h1>
+        <div class='flex' id=''>
+          {}
+        </div>
+      </div>
+  )
+}
+
+export default NewReleases
