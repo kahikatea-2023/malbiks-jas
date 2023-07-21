1f44e5e25cdc03672c4d1f05f4de83ebb5e14c6a
andrew-trotter <andrewjtrotter@gmail.com>
2023-07-21 15:06:48 +1200

    Added content component

---
+++ src/Components/Content.tsx
@@ -0,0 +1,17 @@
+import * as elements from 'typed-html'
+import SideBar from './SideBar'
+import MainContent from './MainContent'
+
+function Content() {
+  return (
+    <div class='bg-white flex mx-20'>
+      
+      <SideBar/>
+ 
+        <MainContent/>
+     
+    </div>
+  )
+}
+
+export default Content
