560f0d1bf4b63bc0cea0f8269ff3817e6a83dcaa
andrew-trotter <andrewjtrotter@gmail.com>
2023-07-22 11:47:36 +1200

    targets next div which is content page

--- src/Components/SearchBar.tsx
+++ src/Components/SearchBar.tsx
@@ -1,11 +1,17 @@
 import * as elements from 'typed-html'
+import SearchPage from './SearchPage'
 
 function SearchBar() {
   return (
-    <form action='/search' class='my-1 mr-1'>
-      <input type="text" name="" id="search-input" class='shadow-lg border-2 border-gray-400 rounded-sm' />
-      <button class='bg-gray-400 text-white px-2 py-1' >Search</button>
-      <button class='bg-gray-400 text-white px-2 py-1'>Advanced</button>
+    <form
+      hx-get="/search"
+      hx-target="next div"
+      hx-swap="outerHTML"
+      class="form my-1 mr-1"
+    >
+      <input type="text" id="search-input" class='shadow-lg border-2 border-gray-400 rounded-sm' />
+      <button class="bg-gray-400 text-white px-2 py-1">Search</button>
+      <button class="bg-gray-400 text-white px-2 py-1">Advanced</button>
     </form>
   )
 }
