658e45ce98a91b874b90320f5523e4de0ff8030a
andrew-trotter <andrewjtrotter@gmail.com>
2023-07-21 17:51:33 +1200

    removed console.log

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -5,7 +5,6 @@ import {data} from "./data"
 async function seed() {
   await db.insert(albums).values(data).returning().get()
   const rows = await db.select().from(albums).all()
-  console.log(rows)
 }
 
 seed()
