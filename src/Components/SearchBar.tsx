9abba1cebd416f90acf3c0ef1f756478dbe9aebd
andrew-trotter <andrewjtrotter@gmail.com>
2023-07-21 15:46:25 +1200

    converted to form

--- src/Components/SearchBar.tsx
+++ src/Components/SearchBar.tsx
@@ -2,12 +2,11 @@ import * as elements from 'typed-html'
 
 function SearchBar() {
   return (
-
-    <div class='my-1 mr-1'>
-      <input type="text" name="" id="" class='shadow-lg border-2 border-gray-400 rounded-sm' />
-      <button class='bg-gray-400 text-white px-2 py-1'>Search</button>
+    <form action='/search' class='my-1 mr-1'>
+      <input type="text" name="" id="search-input" class='shadow-lg border-2 border-gray-400 rounded-sm' />
+      <button class='bg-gray-400 text-white px-2 py-1' >Search</button>
       <button class='bg-gray-400 text-white px-2 py-1'>Advanced</button>
-    </div>
+    </form>
   )
 }
 
