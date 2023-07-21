23229647e31babda589cd8217b40d1c40936ccf9
andrew-trotter <andrewjtrotter@gmail.com>
2023-07-21 15:07:58 +1200

    Added list to sidebar

--- src/Components/SearchBar.tsx
+++ src/Components/SearchBar.tsx
@@ -5,8 +5,8 @@ function SearchBar() {
 
     <div class='my-1 mr-1'>
       <input type="text" name="" id="" class='shadow-lg border-2 border-gray-400 rounded-sm' />
-      <button class='bg-gray-400 px-2 py-1'>Search</button>
-      <button class='bg-gray-400 px-2 py-1'>Advanced</button>
+      <button class='bg-gray-400 text-white px-2 py-1'>Search</button>
+      <button class='bg-gray-400 text-white px-2 py-1'>Advanced</button>
     </div>
   )
 }
---
+++ src/Components/SideBar.tsx
@@ -0,0 +1,36 @@
+import * as elements from 'typed-html'
+
+function Sidebar() {
+  return (
+    <div class='flex flex-col ml-2 bg-white'>
+      <p class='bg-malbik-gray font-bold'>Classical</p>
+      <ul class='flex flex-col'>
+        <li>CD</li>
+        <li>DVD</li>
+        <li>Blu-Ray</li>
+        <li>Chart</li>
+        <li>Best Versions</li>
+        <li>Boxed Sets</li>
+        <li>Naxos 2023</li>
+        <li>releases</li>
+        <li>Classical Award</li>
+        <li>Winners</li>
+        <li>Marbecks</li>
+        <li>Collectables</li>
+        <li>Gift Vouchers</li>
+        <li>Accessories</li>
+        <li>Storage</li>
+      </ul>
+      <p class='bg-malbik-gray'>Info</p>
+      <ul>
+        <li>Info / Help</li>
+        <li>Newsletters</li>
+        <li>About Us</li>
+        <li>Privacy</li>
+      </ul>
+      
+    </div>
+  )
+}
+
+export default Sidebar
