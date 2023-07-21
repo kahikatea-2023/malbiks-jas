fe5b90dc76535830a4f660fa80e854a7d6805ad2
andrew-trotter <andrewjtrotter@gmail.com>
2023-07-21 16:03:50 +1200

    ready to be seeded

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -3,14 +3,15 @@ import { integer, real, sqliteTable, text } from 'drizzle-orm/sqlite-core'
 
 export const albums = sqliteTable('albums', {
   id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
-  title: text('title').notNull(),
   artist: text('artist').notNull(),
-  releaseDate: text('release_date').notNull(),
-  price: real('price').notNull(),
-  format: text('format').notNull(),
-  availability: text('availability'),
+  title: text('title').notNull(),
   img: text('img').notNull(),
+  producer: text('producer').notNull(),
+  format: text('format').notNull(),
+  releaseDate: text('release_date').notNull(),
+  price: integer('price').notNull(),
   description: text('description').notNull(),
+  availability: text('availability'),
 })
 
 export type Album = InferModel<typeof albums>
