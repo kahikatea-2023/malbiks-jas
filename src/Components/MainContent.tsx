331d6e3a7c9b0e6c7824940428372567a37b4093
andrew-trotter <andrewjtrotter@gmail.com>
2023-07-21 15:07:27 +1200

    style: Adding content to main body

---
+++ src/Components/MainContent.tsx
@@ -0,0 +1,36 @@
+import * as elements from 'typed-html'
+import SideBar from './SideBar'
+
+function MainContent() {
+  return (
+    <div class="bg-white ml-2 mb-2 flex flex-col gap-2 w-full border-2 border-gray-400 rounded-md p-2">
+      <div class="p-2 bg-malbik-gray border-2 border-gray-400 rounded-md">
+        <h1 class="text-malbik-red font-bold text-2xl">
+          Our Classical Selection
+        </h1>
+        <p>
+          We are the last true classical music stockist in the country, with
+          thousands of CDs and DVDs in stock and knowledgeable staff to advise
+          and recommend.
+        </p>
+        <h1 class="text-malbik-red font-bold text-xl">
+          FREE DELIVERY within NZ for orders over $100
+        </h1>
+      </div>
+      <div class="border-2 border-gray-400 rounded-md overflow-clip">
+        <img
+          class=""
+          src="https://www.marbecks.co.nz/_ads/2023/2023-04/2023-04-H-long-L.jpg"
+          alt=""
+        />
+      </div>
+      <div class="p-2 bg-malbik-gray border-2 border-gray-400 rounded-md">
+        <h1 class="text-malbik-red font-bold text-xl">
+          Outstanding New Releases
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default MainContent
