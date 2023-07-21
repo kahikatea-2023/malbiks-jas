ceebfe4167606c0db8d5b982a9274eb8647e0c15
Sarah Zhong <sarahchungwu@gmail.com>
2023-07-21 21:22:06 +1200

    fixed the styling for the box

--- src/Components/NewReleases.tsx
+++ src/Components/NewReleases.tsx
@@ -3,16 +3,17 @@ import { Album } from '../db/schema'
 
 function NewReleases({ albums }: { albums: Album[] }) {
   return (
-    <div class="p-2 bg-malbik-gray border-2 border-gray-400 rounded-md">
+    <div class="p-2">
       <h1 class="text-malbik-red font-bold text-xl">
         Outstanding New Releases
       </h1>
-      <div class="flex" id="">
+      <div class="flex flex-row flex-wrap p-5">
         {albums.map((album) => (
-          <div class="w-1/4">
-            <p>{album.title}</p>
+          <div class="w-1/5 bg-malbik-gray border-2 m-2 border-gray-400 rounded-md p-3 flex flex-col items-center">
+            <img class="w-15" src={`${album.img}`} alt={`${album.title}`} />
+            <h1 class="text-malbik-red">{album.title}</h1>
             <p>{album.artist}</p>
-            <p>{album.price}</p>
+            <p>${album.price}</p>
           </div>
         ))}
       </div>
