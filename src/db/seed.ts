355f2a494fbbfa5626b2683e971577f2958f931d
andrew-trotter <andrewjtrotter@gmail.com>
2023-07-21 16:04:02 +1200

    ready when data comes

---
+++ src/db/seed.ts
@@ -0,0 +1,11 @@
+import { db } from './index'
+import { albums } from './schema'
+import data from "./data"
+
+async function seed() {
+  await db.insert(albums).values(data).returning().get()
+  const rows = await db.select().from(albums).all()
+  console.log(rows)
+}
+
+seed()
