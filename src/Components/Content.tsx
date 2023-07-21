e65c478ca96eb0c2af5850e984e6f91ff7bb837a
andrew-trotter <andrewjtrotter@gmail.com>
2023-07-21 15:07:07 +1200

    style: removed whitespace

--- src/Components/Content.tsx
+++ src/Components/Content.tsx
@@ -4,12 +4,9 @@ import MainContent from './MainContent'
 
 function Content() {
   return (
-    <div class='bg-white flex mx-20'>
-      
-      <SideBar/>
- 
-        <MainContent/>
-     
+    <div class="bg-white flex mx-20">
+      <SideBar />
+      <MainContent />
     </div>
   )
 }
