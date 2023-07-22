a2b1f164a4248e6073e2287caad1a4e18052f0da
andrew-trotter <andrewjtrotter@gmail.com>
2023-07-22 12:05:47 +1200

    search page structured with resultItems

--- src/Components/SearchPage.tsx
+++ src/Components/SearchPage.tsx
@@ -1,10 +1,36 @@
 import * as elements from "typed-html"
 import { Album } from "../db/schema"
+import ResultItem from "./ResultItem"
 
-function SearchPage(results: Album[]) {
+interface Props {
+  results: Album[]
+}
+
+function SearchPage({results}: Props) {
+  
   return(
     <div>
-      <p>hi</p>
+      <h2>Displaying items 1 to {results.length}</h2>
+      <div class="flex justify-between bg-malbik-gray">Sort 
+        <ul class="flex flex-row gap-2">
+          <li>Artist</li>
+          <li>Title</li>
+          <li>Price</li>
+          <li>Release Date</li>
+          <li>Format</li>
+          <li>Availability</li>
+        </ul>
+        <ul class="flex flex-row gap-2">Per Page:
+          <li>10</li>
+          <li>20</li>
+          <li>40</li>
+        </ul>
+      </div>
+      <section>
+        {results.map((album, index) => <ResultItem album={album}/>)}
+      </section>
     </div>
   )
 }
+
+export default SearchPage
